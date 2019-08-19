37933b4d98a0df197120dcf0f07a69940725c574
tomo.iwamoto <akirouakirouakirou@yahoo.co.jp>
2019-08-19 08:33:41 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
 
 root to: 'toppages#index'
 
+get 'signup', to: 'users#new'
+resources :users, only: [:new, :show, :create, :index]
+
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
+
+resources :microposts, only: [:create, :destroy]
+
 end 
