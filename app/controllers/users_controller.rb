61c5305a0eb12692c30e205a4854769b830fc9e8
tomo.iwamoto <akirouakirouakirou@yahoo.co.jp>
2019-08-31 05:38:51 +0000

    kansei

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   
   def likes
     @user = User.find(params[:id])
-    @favpost = @user.favposts.page(params[:page])
+    @favposts = @user.favposts.page(params[:page])
     counts(@user)
   end
   
